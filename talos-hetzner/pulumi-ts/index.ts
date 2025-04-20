d686b8e83571bc9c5ade1a0e17e9183efce9705d
juan <juanfbl9307@gmail.com>
2025-04-20 08:32:42 -0500

    comments in code

--- talos-hetzner/pulumi-ts/index.ts
+++ talos-hetzner/pulumi-ts/index.ts
@@ -1,11 +1,11 @@
 import * as talos from "@pulumiverse/talos";
 import * as hcloud from "@pulumi/hcloud";
-
+// configuration variables
 const server_image = "224159775"
 const clusterName = "exampleCluster"
 const numberOfWorkerNodes = 2
 let workerNodes: hcloud.Server[] = [];
-// Create a new server with the specified image from the snapshot created.
+// Server controlplane
 const controlPlane = new hcloud.Server(`controlplane`, {
     name: `controlplane`,
     image: server_image,
@@ -16,6 +16,7 @@ const controlPlane = new hcloud.Server(`controlplane`, {
         ipv6Enabled: true,
     }],
 })
+// Server workers
 for (let i = 0; i < numberOfWorkerNodes; i++) {
     let name = `worker-${i}`;
     const node = new hcloud.Server(name, {
@@ -30,17 +31,19 @@ for (let i = 0; i < numberOfWorkerNodes; i++) {
     });
     workerNodes.push(node);
 }
-
+// Control plane IP
 const controlPlaneIp = controlPlane.ipv4Address
+// Talos
+// Credentials
 const secrets = new talos.machine.Secrets("secrets", {},{dependsOn: [controlPlane]});
-
+// Configuration definition
 const configuration = talos.machine.getConfigurationOutput({
     clusterName: clusterName,
     machineType: "controlplane",
     clusterEndpoint: "https://cluster.local:6443",
     machineSecrets: secrets.machineSecrets,
 },{dependsOn: [secrets]});
-
+// Apply configuration to controlPlane
 const configApplyControlPlane = new talos.machine.ConfigurationApply("configurationApply", {
     clientConfiguration: secrets.clientConfiguration,
     machineConfigurationInput: configuration.machineConfiguration,
@@ -53,14 +56,14 @@ const configApplyControlPlane = new talos.machine.ConfigurationApply("configurat
         },
     })],
 });
-
+// Bootstrap the cluster with controlPlane ipv4Address
 const bootstrap = new talos.machine.Bootstrap("bootstrap", {
     node: controlPlaneIp,
     clientConfiguration: secrets.clientConfiguration,
 }, {
     dependsOn: [configApplyControlPlane],
 });
-
+// Apply configuration to workerNodes
 for (let i = 0; i < workerNodes.length; i++) {
     const workerNodeIp = workerNodes[i].ipv4Address;
     new talos.machine.ConfigurationApply(`configurationApplyWorker-${i}`, {
@@ -78,8 +81,8 @@ for (let i = 0; i < workerNodes.length; i++) {
         dependsOn: [bootstrap],
     });
 }
-
-const kubeconfig = new talos.cluster.Kubeconfig("kubeconfig", {
+// Retriving kubeconfig output
+const kubeConfig = new talos.cluster.Kubeconfig("kubeconfig", {
     node: controlPlaneIp,
     clientConfiguration: {
         caCertificate: secrets.clientConfiguration.caCertificate,
@@ -87,5 +90,5 @@ const kubeconfig = new talos.cluster.Kubeconfig("kubeconfig", {
         clientKey: secrets.clientConfiguration.clientKey,
     }
 })
-
-export const kubeconfigOutput = kubeconfig.kubeconfigRaw;
+// Stack output defintion
+export const kubeconfig = kubeConfig.kubeconfigRaw;
